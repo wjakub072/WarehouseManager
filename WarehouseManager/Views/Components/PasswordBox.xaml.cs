4b6b2571e935508d00d2f7d328e7c2fb00b6e326
Jakub Włodarczyk <wjakub072@gmail.com>
2022-03-17 19:40:45 +0100

    LoginView

---
+++ WarehouseManager/Views/Components/PasswordBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarehouseManager.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for PasswordBox.xaml
+    /// </summary>
+    public partial class PasswordBox : UserControl
+    {
+        public string PasswordProperty
+        {
+            get { return (string)GetValue(PasswordPropertyProperty); }
+            set { SetValue(PasswordPropertyProperty, value); }
+        }
+
+       
+
+        public static readonly DependencyProperty PasswordPropertyProperty =
+            DependencyProperty.Register("PasswordProperty", typeof(string), typeof(PasswordBox), new PropertyMetadata(string.Empty));
+
+
+
+        public PasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs eventArgs)
+        {
+            PasswordProperty = passwordBox.Password;
+        }
+    }
+}
