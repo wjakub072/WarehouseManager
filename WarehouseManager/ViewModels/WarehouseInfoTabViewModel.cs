27dd1d9d98c8972f73e34e2aea5fc95c535e7404
Jakub Włodarczyk <wjakub072@gmail.com>
2022-04-25 20:55:28 +0200

    SectorInfo Set

--- WarehouseManager/ViewModels/WarehouseInfoTabViewModel.cs
+++ WarehouseManager/ViewModels/WarehouseInfoTabViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+using WarehouseManager.Data;
+using WarehouseManager.Models;
 
 namespace WarehouseManager.ViewModels
 {
     internal class WarehouseInfoTabViewModel : ViewModelBase
     {
+        private string _warehouse;
+
+        public string Warehouse
+        {
+            get { return _warehouse; }
+            set { _warehouse = value; }
+        }
+
+        private string _operator;
+
+        public string Operator
+        {
+            get { return _operator; }
+            set { _operator = value; }
+        }
+
+        private ObservableCollection<Sector> _sectors;
+
+        public ObservableCollection<Sector> Sectors
+        {
+            get { return _sectors; }
+            set { _sectors = value; }
+        }
+
+
+        private readonly ApplicationDatabaseContext _db;
+
+        public WarehouseInfoTabViewModel(ApplicationDatabaseContext db)
+        {
+            _db = db;
+
+            _sectors = new ObservableCollection<Sector>((System.Collections.Generic.IEnumerable<Sector>)_db.Sectors.ToListAsync());
+        }
     }
 }
